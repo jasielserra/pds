c9661eff52de593515d026ad957d69e5c9519fe5
jasiel <jasiel_serra@hotmail.com>
2022-04-14 22:16:43 -0300

    getter heavier apple

---
+++ scratch.py
@@ -0,0 +1,20 @@
+apples = [20,4,5,10,15]
+
+class Sorted:
+    def __init__(self, items):
+        self.data = items
+
+    def sort(self):
+        self.data.sort()
+
+    def get(self, index):
+        return self.data[index]
+
+def consumer1(cesta):
+    """ Pega a maçã mais pesada."""
+
+    s = Sorted(cesta)
+    s.sort()
+    return s.get(-1)
+
+print(consumer1(apples))
